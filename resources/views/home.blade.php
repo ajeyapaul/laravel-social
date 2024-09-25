f2a4722d78e77fb8cb4692f4ddd3632afc083186
Ajeya Paul <paulajeya@gmail.com>
2024-09-25 18:09:24 +0530

    added support for laravel livewire

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+<x-app-layout>
+    <div class="py-12">
+        <div class="max-w-3xl mx-auto sm:px-3 lg:px-3">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    {{ __("Welcome!!") }}
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->middleware('auth')->name('home');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
